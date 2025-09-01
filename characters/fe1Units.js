82608265d79f4cac80884d8deaa794b36c9df325
Adam Carleton <acarleton15@gmail.com>
2025-08-31 19:33:35 -0500

    folder for holding character objects by game

---
+++ characters/fe1Units.js
@@ -0,0 +1,16 @@
+import { startTransition } from "react";
+import { shadowDragon } from "../public/Paths.js";
+
+const FE1Units = [
+    {
+        name: "Marth",
+        startingClass: "Lord",
+        moveType: "Infantry",
+        game: "Shadow Dragon",
+        img1: `${shadowDragon}marth.png`,
+        img2: `${shadowDragon}marth2.png`,
+        isFound: false
+    },
+];
+
+export default FE1Units;
