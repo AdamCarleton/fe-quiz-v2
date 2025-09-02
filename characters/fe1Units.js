c6d02c3bbe75009223e7f47259e418086c954ad5
Adam Carleton <acarleton15@gmail.com>
2025-09-02 17:41:59 -0500

    Added more characters for testing

--- characters/fe1Units.js
+++ characters/fe1Units.js
@@ -10,6 +10,33 @@ const FE1Units = [
         img2: `${shadowDragon}marth2.png`,
         isFound: false
     },
+    {
+        name: "Jagen",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Shadow Dragon",
+        img1: `${shadowDragon}jagen.png`,
+        img2: `${shadowDragon}jagen2.png`,
+        isFound: false
+    },
+    {
+        name: "Abel",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Shadow Dragon",
+        img1: `${shadowDragon}abel.png`,
+        img2: `${shadowDragon}abel2.png`,
+        isFound: false
+    },
+    {
+        name: "Cain",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Shadow Dragon",
+        img1: `${shadowDragon}cain.png`,
+        img2: `${shadowDragon}cain2.png`,
+        isFound: false
+    }
 ];
 
 export default FE1Units;
---
+++ characters/fe3Units.js
@@ -0,0 +1,24 @@
+import { newMystery } from "../public/Paths.js";
+
+const FE3Units = [
+    {
+        name: "Marth",
+        startingClass: "Lord",
+        moveType: "Infantry",
+        game: "New Mystery of the Emblem",
+        img1: `${newMystery}marth.png`,
+        img2: `${newMystery}marth2.png`,
+        isFound: false
+    },
+    {
+        name: "Kris",
+        startingClass: "Varies",
+        moveType: "Infantry",
+        game: "New Mystery of the Emblem",
+        img1: `${newMystery}kris.png`,
+        img2: `${newMystery}kris2.png`,
+        isFound: false
+    }
+];
+
+export default FE3Units;
