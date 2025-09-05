5f7a2f0dab413d1dc8c2fe057286af8513b129ab
Adam Carleton <acarleton15@gmail.com>
2025-09-05 16:51:40 -0500

    Finished playable adding characters for shadows of valentia

--- characters/fe2Units.js
+++ characters/fe2Units.js
@@ -12,6 +12,95 @@ const FE2Units = [
         isFound: false
     },
     {
+        name: "Lukas",
+        startingClass: "Soldier",
+        moveType: "Armor",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}lukas.png`,
+        img2: `${echoes}lukas2.png`,
+        isFound: false
+    },
+    {
+        name: "Gray",
+        startingClass: "Villager",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}gray.png`,
+        img2: `${echoes}gray2.png`,
+        isFound: false
+    },
+    {
+        name: "Tobin",
+        startingClass: "Villager",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}tobin.png`,
+        img2: `${echoes}tobin2.png`,
+        isFound: false
+    },
+    {
+        name: "Kliff",
+        startingClass: "Villager",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}kliff.png`,
+        img2: `${echoes}kliff2.png`,
+        isFound: false
+    },
+    {
+        name: "Faye",
+        startingClass: "Villager",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}faye.png`,
+        isFound: false
+    },
+    {
+        name: "Silque",
+        startingClass: "Cleric",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}silque.png`,
+        img2: `${echoes}silque2.png`,
+        isFound: false
+    },
+    {
+        name: "Clair",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}clair.png`,
+        img2: `${echoes}clair2.png`,
+        isFound: false
+    },
+    {
+        name: "Clive",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}clive.png`,
+        img2: `${echoes}clive2.png`,
+        isFound: false
+    },
+    {
+        name: "Forsyth",
+        startingClass: "Soldier",
+        moveType: "Armor",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}forsyth.png`,
+        img2: `${echoes}forsyth2.png`,
+        isFound: false
+    },
+    {
+        name: "Python",
+        startingClass: "Horseman",
+        moveType: "Cavalry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}python.png`,
+        img2: `${echoes}python2.png`,
+        isFound: false
+    },
+    {
         name: "Celica",
         startingClass: "Priestess",
         moveType: "Infantry",
@@ -20,7 +109,250 @@ const FE2Units = [
         img2: `${echoes}celica2.png`,
         img3: `${echoes}celica3.png`,
         isFound: false
-    }
+    },
+    {
+        name: "Mae",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}mae.png`,
+        img2: `${echoes}mae2.png`,
+        isFound: false
+    },
+    {
+        name: "Boey",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}boey.png`,
+        img2: `${echoes}boey2.png`,
+        isFound: false
+    },
+    {
+        name: "Genny",
+        startingClass: "Cleric",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}genny.png`,
+        img2: `${echoes}genny2.png`,
+        isFound: false
+    },
+    {
+        name: "Saber",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}saber.png`,
+        img2: `${echoes}saber2.png`,
+        isFound: false
+    },
+    {
+        name: "Valbar",
+        startingClass: "Knight",
+        moveType: "Armored",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}valbar.png`,
+        img2: `${echoes}valbar2.png`,
+        isFound: false
+    },
+    {
+        name: "Kamui",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}kamui.png`,
+        img2: `${echoes}kamui2.png`,
+        isFound: false
+    },
+    {
+        name: "Leon",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}leon.png`,
+        img2: `${echoes}leon2.png`,
+        isFound: false
+    },
+    {
+        name: "Luthier",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}luthier.png`,
+        img2: `${echoes}luthier2.png`,
+        isFound: false
+    },
+    {
+        name: "Mathilda",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}mathilda.png`,
+        img2: `${echoes}mathilda2.png`,
+        isFound: false
+    },
+    {
+        name: "Delthea",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}delthea.png`,
+        img2: `${echoes}delthea2.png`,
+        isFound: false
+    },
+    {
+        name: "Palla",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}palla.png`,
+        img2: `${echoes}palla2.png`,
+        isFound: false
+    },
+    {
+        name: "Catria",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}catria.png`,
+        img2: `${echoes}catria2.png`,
+        isFound: false
+    },
+    {
+        name: "Atlas",
+        startingClass: "Villager",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}atlas.png`,
+        img2: `${echoes}atlas2.png`,
+        isFound: false
+    },
+    {
+        name: "Jesse",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}jesse.png`,
+        img2: `${echoes}jesse2.png`,
+        isFound: false
+    },
+    {
+        name: "Sonya",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}sonya.png`,
+        img2: `${echoes}sonya2.png`,
+        isFound: false
+    },
+    {
+        name: "Deen",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}deen.png`,
+        img2: `${echoes}deen2.png`,
+        isFound: false
+    },
+    {
+        name: "Est",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}est.png`,
+        img2: `${echoes}est2.png`,
+        isFound: false
+    },
+    {
+        name: "Tatiana",
+        startingClass: "Priestess",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}tatiana.png`,
+        img2: `${echoes}tatiana2.png`,
+        isFound: false
+    },
+    {
+        name: "Zeke",
+        startingClass: "Gold Knight",
+        moveType: "Cavalry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}zeke.png`,
+        img2: `${echoes}zeke2.png`,
+        isFound: false
+    },
+    {
+        name: "Nomah",
+        startingClass: "Sage",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}nomah.png`,
+        img2: `${echoes}nomah2.png`,
+        isFound: false
+    },
+    {
+        name: "Conrad",
+        startingClass: "Paladin",
+        moveType: "Calvary",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}conrad.png`,
+        img2: `${echoes}conrad2.png`,
+        isFound: false
+    },
+    {
+        name: "Mycen",
+        startingClass: "Gold Knight",
+        moveType: "Cavalry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}mycen.png`,
+        img2: `${echoes}mycen2.png`,
+        isFound: false
+    },
+    {
+        name: "Fernand",
+        startingClass: "Cavalier",
+        moveType: "Calvary",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}fernand.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Emma",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}emma.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Randal",
+        startingClass: "Wyvern Knight",
+        moveType: "Flying",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}randal.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Yuzu",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}yuzu.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Shade",
+        startingClass: "Sage",
+        moveType: "Infantry",
+        game: "Echoes: Shadows of Valentia",
+        img1: `${echoes}shade.png`,
+        isFound: false,
+        isDLC: true
+    },
 ];
 
 export default FE2Units;
