dd455ae780641712ea30e5f0772f6ac555e182c8
Adam Carleton <acarleton15@gmail.com>
2025-09-07 12:44:29 -0500

    added pre-time-skip characters

--- characters/fe4Units.js
+++ characters/fe4Units.js
@@ -3,15 +3,199 @@ import { genealogy } from "../public/Paths.js";
 const FE4Units = [
     {
         name: "Sigurd",
-        startingClass: "Lord",
+        startingClass: "Knight Lord",
         moveType: "Cavalry",
         game: "Genealogy of the Holy War",
         img1: `${genealogy}sigurd.png`,
         isFound: false
     },
     {
+        name: "Alec",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}alec.png`,
+        isFound: false
+    },
+    {
+        name: "Naoise",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}naoise.png`,
+        isFound: false
+    },
+    {
+        name: "Arden",
+        startingClass: "Sword Armor",
+        moveType: "Armor",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}arden.png`,
+        isFound: false
+    },
+    {
+        name: "Azelle",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}azelle.png`,
+        isFound: false
+    },
+    {
+        name: "Lex",
+        startingClass: "Axe Knight",
+        moveType: "Cavalry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}lex.png`,
+        isFound: false
+    },
+    {
+        name: "Quan",
+        startingClass: "Duke Knight",
+        moveType: "Cavalry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}quan.png`,
+        isFound: false
+    },
+    {
+        name: "Ethlyn",
+        startingClass: "Troubadour",
+        moveType: "Cavalry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}ethlyn.png`,
+        isFound: false
+    },
+    {
+        name: "Finn",
+        startingClass: "Lance Knight",
+        moveType: "Cavalry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}finn.png`,
+        isFound: false
+    },
+    {
+        name: "Midir",
+        startingClass: "Arch Knight",
+        moveType: "Cavalry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}midir.png`,
+        isFound: false
+    },
+    {
+        name: "Edain",
+        startingClass: "Priest",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}edain.png`,
+        isFound: false
+    },
+    {
+        name: "Dew",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}dew.png`,
+        isFound: false
+    },
+    {
+        name: "Ayra",
+        startingClass: "Sword Fighter",
+        moveType: "Cavalry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}ayra.png`,
+        isFound: false
+    },
+    {
+        name: "Deirdre",
+        startingClass: "Shaman",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}deirdre.png`,
+        isFound: false
+    },
+    {
+        name: "Jamke",
+        startingClass: "Bow Fighter",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}jamke.png`,
+        isFound: false
+    },
+    {
+        name: "Chulainn",
+        startingClass: "Sword Fighter",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}chulainn.png`,
+        isFound: false
+    },
+    {
+        name: "Lachesis",
+        startingClass: "Princess",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}lachesis.png`,
+        isFound: false
+    },
+    {
+        name: "Beowolf",
+        startingClass: "Free Knight",
+        moveType: "Cavalry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}beowolf.png`,
+        isFound: false
+    },
+    {
+        name: "Lewyn",
+        startingClass: "Bard",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}lewyn.png`,
+        isFound: false
+    },
+    {
+        name: "Silvia",
+        startingClass: "Dancer",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}silvia.png`,
+        isFound: false
+    },
+    {
+        name: "Erinys",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}erinys.png`,
+        isFound: false
+    },
+    {
+        name: "Brigid",
+        startingClass: "Sniper",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}brigid.png`,
+        isFound: false
+    },
+    {
+        name: "Claud",
+        startingClass: "High Priest",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}claud.png`,
+        isFound: false
+    },
+    {
+        name: "Tailtiu",
+        startingClass: "Thunder Mage",
+        moveType: "Infantry",
+        game: "Genealogy of the Holy War",
+        img1: `${genealogy}tailtiu.png`,
+        isFound: false
+    },
+    {
         name: "Seliph",
-        startingClass: "Lord",
+        startingClass: "Junior Lord",
         moveType: "Cavalry",
         game: "Genealogy of the Holy War",
         img1: `${genealogy}seliph.png`,
