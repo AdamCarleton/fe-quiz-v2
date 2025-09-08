cfc03622a6082a8d1fb9bb5fe719cd07aeb046be
Adam Carleton <acarleton15@gmail.com>
2025-09-08 18:47:09 -0500

    finished adding fe6 characters including post-game trial map characters

--- characters/fe6Units.js
+++ characters/fe6Units.js
@@ -8,7 +8,503 @@ const FE6Units = [
         game: "Binding Blade",
         img1: `${bindingBlade}roy.png`,
         isFound: false
-    }
+    },
+    {
+        name: "Marcus",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}marcus.png`,
+        isFound: false
+    },
+    {
+        name: "Alen",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}alen.png`,
+        isFound: false
+    },
+    {
+        name: "Lance",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}lance.png`,
+        isFound: false
+    },
+    {
+        name: "Wolt",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}wolt.png`,
+        isFound: false
+    },
+    {
+        name: "Bors",
+        startingClass: "Archer",
+        moveType: "Armor",
+        game: "Binding Blade",
+        img1: `${bindingBlade}bors.png`,
+        isFound: false
+    },
+    {
+        name: "Merlinus",
+        startingClass: "Transporter",
+        moveType: "Other",
+        game: "Binding Blade",
+        img1: `${bindingBlade}merlinus.png`,
+        isFound: false
+    },
+    {
+        name: "Elen",
+        startingClass: "Cleric",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}elen.png`,
+        isFound: false
+    },
+    {
+        name: "Dieck",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}dieck.png`,
+        isFound: false
+    },
+    {
+        name: "Lot",
+        startingClass: "Fighter",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}lot.png`,
+        isFound: false
+    },
+    {
+        name: "Wade",
+        startingClass: "Fighter",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}wade.png`,
+        isFound: false
+    },
+    {
+        name: "Shanna",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Binding Blade",
+        img1: `${bindingBlade}shanna.png`,
+        isFound: false
+    },
+    {
+        name: "Chad",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}chad.png`,
+        isFound: false
+    },
+    {
+        name: "Lugh",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}lugh.png`,
+        isFound: false
+    },
+    {
+        name: "Clarine",
+        startingClass: "Troubadour",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}clarine.png`,
+        isFound: false
+    },
+    {
+        name: "Rutger",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}rutger.png`,
+        isFound: false
+    },
+    {
+        name: "Saul",
+        startingClass: "Priest",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}saul.png`,
+        isFound: false
+    },
+    {
+        name: "Dorothy",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}dorothy.png`,
+        isFound: false
+    },
+    {
+        name: "Sue",
+        startingClass: "Nomad",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}sue.png`,
+        isFound: false
+    },
+    {
+        name: "Zealot",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}zealot.png`,
+        isFound: false
+    },
+    {
+        name: "Trec",
+        startingClass: "Cavalier",
+        moveType: "Calvary",
+        game: "Binding Blade",
+        img1: `${bindingBlade}trec.png`,
+        isFound: false
+    },
+    {
+        name: "Noah",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}noah.png`,
+        isFound: false
+    },
+    {
+        name: "Astolfo",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}astolfo.png`,
+        isFound: false
+    },
+    {
+        name: "Lilina",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}lilina.png`,
+        isFound: false
+    },
+    {
+        name: "Gwendolyn",
+        startingClass: "Knight",
+        moveType: "Armor",
+        game: "Binding Blade",
+        img1: `${bindingBlade}gwendolyn.png`,
+        isFound: false
+    },
+    {
+        name: "Barthe",
+        startingClass: "Knight",
+        moveType: "Armory",
+        game: "Binding Blade",
+        img1: `${bindingBlade}barthe.png`,
+        isFound: false
+    },
+    {
+        name: "Ogier",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}ogier.png`,
+        isFound: false
+    },
+    {
+        name: "Fir",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}fir.png`,
+        isFound: false
+    },
+    {
+        name: "Sin",
+        startingClass: "Nomad",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}sin.png`,
+        isFound: false
+    },
+    {
+        name: "Gonzalez",
+        startingClass: "Brigand",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}gonzalez.png`,
+        isFound: false
+    },
+    {
+        name: "Geese",
+        startingClass: "Pirate",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}geese.png`,
+        isFound: false
+    },
+    {
+        name: "Klein",
+        startingClass: "Sniper",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}klein.png`,
+        isFound: false
+    },
+    {
+        name: "Thea",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Binding Blade",
+        img1: `${bindingBlade}thea.png`,
+        isFound: false
+    },
+    {
+        name: "Larum",
+        startingClass: "Dancer",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}larum.png`,
+        isFound: false
+    },
+    {
+        name: "Echidna",
+        startingClass: "Hero",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}echidna.png`,
+        isFound: false
+    },
+    {
+        name: "Elffin",
+        startingClass: "Bard",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}elffin.png`,
+        isFound: false
+    },
+    {
+        name: "Bartre",
+        startingClass: "Warrior",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}bartre.png`,
+        isFound: false
+    },
+    {
+        name: "Raigh",
+        startingClass: "Shaman",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}raigh.png`,
+        isFound: false
+    },
+    {
+        name: "Cath",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}cath.png`,
+        isFound: false
+    },
+    {
+        name: "Melady",
+        startingClass: "Wyvern Rider",
+        moveType: "Flying",
+        game: "Binding Blade",
+        img1: `${bindingBlade}melady.png`,
+        isFound: false
+    },
+    {
+        name: "Perceval",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}perceval.png`,
+        isFound: false
+    },
+    {
+        name: "Cecilia",
+        startingClass: "Valkyrie",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}cecilia.png`,
+        isFound: false
+    },
+    {
+        name: "Sophia",
+        startingClass: "Shaman",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}sophia.png`,
+        isFound: false
+    },
+    {
+        name: "Igrene",
+        startingClass: "Sniper",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}igrene.png`,
+        isFound: false
+    },
+    {
+        name: "Garrett",
+        startingClass: "Berserker",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}garrett.png`,
+        isFound: false
+    },
+    {
+        name: "Fae",
+        startingClass: "manakete",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}fae.png`,
+        isFound: false
+    },
+    {
+        name: "Hugh",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}hugh.png`,
+        isFound: false
+    },
+    {
+        name: "Zeiss",
+        startingClass: "Wyvern Rider",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}zeiss.png`,
+        isFound: false
+    },
+    {
+        name: "Douglas",
+        startingClass: "General",
+        moveType: "Armor",
+        game: "Binding Blade",
+        img1: `${bindingBlade}douglas.png`,
+        isFound: false
+    },
+    {
+        name: "Niime",
+        startingClass: "Druid",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}niime.png`,
+        isFound: false
+    },
+    {
+        name: "Dayan",
+        startingClass: "Nomadic Trooper",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}dayan.png`,
+        isFound: false
+    },
+    {
+        name: "Juno",
+        startingClass: "Falcon Knight",
+        moveType: "Flying",
+        game: "Binding Blade",
+        img1: `${bindingBlade}juno.png`,
+        isFound: false
+    },
+    {
+        name: "Yoder",
+        startingClass: "Bishop",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}yoder.png`,
+        isFound: false
+    },
+    {
+        name: "Karel",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}karel.png`,
+        isFound: false
+    },
+    {
+        name: "Narcian",
+        startingClass: "Wyvern Lord",
+        moveType: "Flying",
+        game: "Binding Blade",
+        img1: `${bindingBlade}narcian.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Galle",
+        startingClass: "Wyvern Lord",
+        moveType: "Flying",
+        game: "Binding Blade",
+        img1: `${bindingBlade}galle.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Hector",
+        startingClass: "General",
+        moveType: "Armor",
+        game: "Binding Blade",
+        img1: `${bindingBlade}hector.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Brunnya",
+        startingClass: "Sage",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}brunnya.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Eliwood",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}eliwood.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Murdock",
+        startingClass: "General",
+        moveType: "Armor",
+        game: "Binding Blade",
+        img1: `${bindingBlade}murdock.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Zephiel",
+        startingClass: "King",
+        moveType: "Armor",
+        game: "Binding Blade",
+        img1: `${bindingBlade}zephiel.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Guinivere",
+        startingClass: "Sage",
+        moveType: "Infantry",
+        game: "Binding Blade",
+        img1: `${bindingBlade}guinivere.png`,
+        isFound: false,
+        isPostGame: true
+    },
 ];
 
 export default FE6Units;
