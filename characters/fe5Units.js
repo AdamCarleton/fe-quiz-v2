f02120ccea3b8295d82d29f4918c66884e1ccee3
Adam Carleton <acarleton15@gmail.com>
2025-09-08 14:18:24 -0500

    finished adding fe5 characters

--- characters/fe5Units.js
+++ characters/fe5Units.js
@@ -8,7 +8,415 @@ const FE5Units = [
         game: "Thracia 776",
         img1: `${thracia}leif.png`,
         isFound: false
-    }
+    },
+    {
+        name: "Finn",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}finn.png`,
+        isFound: false
+    },
+    {
+        name: "Osian",
+        startingClass: "Axe Fighter",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}osian.png`,
+        isFound: false   
+    },
+    {
+        name: "Halvan",
+        startingClass: "Axe Fighter",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}halvan.png`,
+        isFound: false
+    },
+    {
+        name: "Eyvel",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}eyvel.png`,
+        isFound: false
+    },
+    {
+        name: "Dagdar",
+        startingClass: "Warrior",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}dagdar.png`,
+        isFound: false
+    },
+    {
+        name:"Tanya",
+        startingClass: "Bow Fighter",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}tanya.png`,
+        isFound: false
+    },
+    {
+        name: "Marty",
+        startingClass: "Brigand",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}marty.png`,
+        isFound: false
+    },
+    {
+        name: "Ronan",
+        startingClass: "Bow Fighter",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}ronan.png`,
+        isFound: false
+    },
+    {
+        name: "Safy",
+        startingClass: "Priest",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}safy.png`,
+        isFound: false
+    },
+    {
+        name: "Lifis",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}lifis.png`,
+        isFound: false
+    },
+    {
+        name: "Machyua",
+        startingClass: "Sword Fighter",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}machyua.png`,
+        isFound: false
+    },
+    {
+        name: "Brighton",
+        startingClass: "Axe Knight",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}brighton.png`,
+        isFound: false
+    },
+    {
+        name: "Lara",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}lara.png`,
+        isFound: false
+    },
+    {
+        name: "Fergus",
+        startingClass: "Free Knight",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}fergus.png`,
+        isFound: false
+    },
+    {
+        name: "Karin",
+        startingClass: "Pegasus Rider",
+        moveType: "Flying",
+        game: "Thracia 776",
+        img1: `${thracia}karin.png`,
+        isFound: false
+    },
+    {
+        name: "Dalsin",
+        startingClass: "Axe Armor",
+        moveType: "Armor",
+        game: "Thracia 776",
+        img1: `${thracia}dalsin.png`,
+        isFound: false
+    },
+    {
+        name: "Asbel",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}asbel.png`,
+        isFound: false
+    },
+    {
+        name: "Nanna",
+        startingClass: "Troubadour",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}nanna.png`,
+        isFound: false
+    },
+    {
+        name: "Hicks",
+        startingClass: "Axe Knight",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}hicks.png`,
+        isFound: false
+    },
+    {
+        name: "Shiva",
+        startingClass: "Sword Fighter",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}shiva.png`,
+        isFound: false
+    },
+    {
+        name: "Carrion",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}carrion.png`,
+        isFound: false
+    },
+    {
+        name: "Selphina",
+        startingClass: "Arch Knight",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}selphina.png`,
+        isFound: false
+    },
+    {
+        name: "Cain",
+        startingClass: "Lance Knight",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}cain.png`,
+        isFound: false
+    },
+    {
+        name: "Alva",
+        startingClass: "Lance Knight",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}alva.png`,
+        isFound: false
+    },
+    {
+        name: "Robert",
+        startingClass: "Arch Knight",
+        moveType: "Cavalier",
+        game: "Thracia 776",
+        img1: `${thracia}robert.png`,
+        isFound: false
+    },
+    {
+        name: "Fred",
+        startingClass: "Paladin",
+        moveType: "Cavalier",
+        game: "Thracia 776",
+        img1: `${thracia}fred.png`,
+        isFound: false
+    },
+    {
+        name: "Olwen",
+        startingClass: "Mage Knight",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}olwen.png`,
+        isFound: false
+    },
+    {
+        name: "Mareeta",
+        startingClass: "Sword Fighter",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}mareeta.png`,
+        isFound: false
+    },
+    {
+        name: "Salem",
+        startingClass: "Loptr Mage",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}Salem.png`,
+        isFound: false
+    },
+    {
+        name: "Perne",
+        startingClass: "Thief Fighter",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}perne.png`,
+        isFound: false
+    },
+    {
+        name: "Troude",
+        startingClass: "Sword Fighter",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}troude.png`,
+        isFound: false
+    },
+    {
+        name: "Tina",
+        startingClass: "Priest",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}tina.png`,
+        isFound: false
+    },
+    {
+        name: "Glade",
+        startingClass: "Duke Knight",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}glade.png`,
+        isFound: false
+    },
+    {
+        name: "Deen",
+        startingClass: "Wyvern Knight",
+        moveType: "Flying",
+        game: "Thracia 776",
+        img1: `${thracia}deen.png`,
+        isFound: false
+    },
+    {
+        name: "Eda",
+        startingClass: "Dragon Rider",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}eda.png`,
+        isFound: false
+    },
+    {
+        name: "Homer",
+        startingClass: "Bard",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}homer.png`,
+        isFound: false
+    },
+    {
+        name: "Linoan",
+        startingClass: "Cleric",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}linoan.png`,
+        isFound: false
+    },
+    {
+        name: "Ralf",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}ralf.png`,
+        isFound: false
+    },
+    {
+        name: "Ilios",
+        startingClass: "Mage Knight",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}ilios.png`,
+        isFound: false
+    },
+    {
+        name: "Sleuf",
+        startingClass: "Priest",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}sleuf.png`,
+        isFound: false
+    },
+    {
+        name: "Misha",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Thracia 776",
+        img1: `${thracia}misha.png`,
+        isFound: false
+    },
+    {
+        name: "Sara",
+        startingClass: "Cleric",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}sara.png`,
+        isFound: false
+    },
+    {
+        name: "Shannam",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}shannam.png`,
+        isFound: false
+    },
+    {
+        name: "Miranda",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}miranda.png`,
+        isFound: false
+    },
+    {
+        name: "Xavier",
+        startingClass: "General",
+        moveType: "Armor",
+        game: "Thracia 776",
+        img1: `${thracia}xavier.png`,
+        isFound: false
+    },
+    {
+        name: "Amalda",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}amalda.png`,
+        isFound: false
+    },
+    {
+        name: "Conomor",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}conomor.png`,
+        isFound: false
+    },
+    {
+        name: "Diarmuid",
+        startingClass: "Ranger",
+        moveType: "Cavalry",
+        game: "Thracia 776",
+        img1: `${thracia}diarmuid.png`,
+        isFound: false
+    },
+    {
+        name: "Saias",
+        startingClass: "High Priest",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}saias.png`,
+        isFound: false
+    },
+    {
+        name: "Ced",
+        startingClass: "Sage",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}ced.png`,
+        isFound: false
+    },
+    {
+        name: "Galzus",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Thracia 776",
+        img1: `${thracia}galzus.png`,
+        isFound: false
+    },
 ];
 
 export default FE5Units;
