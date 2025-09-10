f17114f41e0d5c6fb6fdf1749f055bfdbc2b5b56
Adam Carleton <acarleton15@gmail.com>
2025-09-10 17:00:04 -0500

    finished adding fe8 characters

--- characters/fe8Units.js
+++ characters/fe8Units.js
@@ -11,6 +11,110 @@ const FE8Units = [
         isFound: false
     },
     {
+        name: "Seth",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}seth.png`,
+        isFound: false
+    },
+    {
+        name: "Franz",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}franz.png`,
+        isFound: false
+    },
+    {
+        name: "Gilliam",
+        startingClass: "Knight",
+        moveType: "Armor",
+        game: "Sacred Stones",
+        img1: `${sacredStones}gilliam.png`,
+        isFound: false
+    },
+    {
+        name: "Vanessa",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Sacred Stones",
+        img1: `${sacredStones}vanessa.png`,
+        isFound: false
+    },
+    {
+        name: "Moulder",
+        startingClass: "Priest",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}moulder.png`,
+        isFound: false
+    },
+    {
+        name: "Ross",
+        startingClass: "Journeyman",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}ross.png`,
+        isFound: false
+    },
+    {
+        name: "Garcia",
+        startingClass: "Fighter",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}garcia.png`,
+        isFound: false
+    },
+    {
+        name: "Neimi",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}neimi.png`,
+        isFound: false
+    },
+    {
+        name: "Colm",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}colm.png`,
+        isFound: false
+    },
+    {
+        name: "Artur",
+        startingClass: "Monk",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}artur.png`,
+        isFound: false
+    },
+    {
+        name: "Lute",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}lute.png`,
+        isFound: false
+    },
+    {
+        name: "Natasha",
+        startingClass: "Cleric",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}natasha.png`,
+        isFound: false
+    },
+    {
+        name: "Joshua",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}joshua.png`,
+        isFound: false
+    },
+    {
         name: "Ephraim",
         startingClass: "Lord",
         moveType: "Infantry",
@@ -18,7 +122,243 @@ const FE8Units = [
         img1: `${sacredStones}ephraim.png`,
         img2: `${sacredStones}ephraim2.png`,
         isFound: false
-    }
+    },
+    {
+        name: "Forde",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}forde.png`,
+        isFound: false
+    },
+    {
+        name: "Kyle",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}kyle.png`,
+        isFound: false
+    },
+    {
+        name: "Orson",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}orson.png`,
+        isFound: false
+    },
+    {
+        name: "Tana",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Sacred Stones",
+        img1: `${sacredStones}tana.png`,
+        isFound: false
+    },
+    {
+        name: "Amelia",
+        startingClass: "Recruit",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}amelia.png`,
+        isFound: false
+    },
+    {
+        name: "Innes",
+        startingClass: "Sniper",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}innes.png`,
+        isFound: false
+    },
+    {
+        name: "Gerik",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}gerik.png`,
+        isFound: false
+    },
+    {
+        name: "Tethys",
+        startingClass: "Dancer",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}tethys.png`,
+        isFound: false
+    },
+    {
+        name: "Marisa",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}marisa.png`,
+        isFound: false
+    },
+    {
+        name: "Larachel",
+        displayName: "L'Arachel",
+        startingClass: "Troubadour",
+        moveType: "Cavalry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}larachel.png`,
+        isFound: false
+    },
+    {
+        name: "Dozla",
+        startingClass: "Berserker",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}dozla.png`,
+        isFound: false
+    },
+    {
+        name: "Saleh",
+        startingClass: "Sage",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}saleh.png`,
+        isFound: false
+    },
+    {
+        name: "Ewan",
+        startingClass: "Pupil",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}ewan.png`,
+        isFound: false
+    },
+    {
+        name: "Cormag",
+        startingClass: "Wyvern Rider",
+        moveType: "Flying",
+        game: "Sacred Stones",
+        img1: `${sacredStones}cormag.png`,
+        isFound: false
+    },
+    {
+        name: "Rennac",
+        startingClass: "Rogue",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}rennac.png`,
+        isFound: false
+    },
+    {
+        name: "Duessel",
+        startingClass: "Great Knight",
+        moveType: "Cavalry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}duessel.png`,
+        isFound: false
+    },
+    {
+        name: "Knoll",
+        startingClass: "Shaman",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}knoll.png`,
+        img2: `${sacredStones}knoll2.png`,
+        isFound: false
+    },
+    {
+        name: "Myrrh",
+        startingClass: "Manakete",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}myrrh.png`,
+        isFound: false
+    },
+    {
+        name: "Syrene",
+        startingClass: "Falcon Knight",
+        moveType: "Flying",
+        game: "Sacred Stones",
+        img1: `${sacredStones}syrene.png`,
+        isFound: false
+    },
+    {
+        name: "Caellach",
+        startingClass: "Hero",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}caellach.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Riev",
+        startingClass: "Bishop",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}riev.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Ismaire",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}ismaire.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Selena",
+        startingClass: "Mage Knight",
+        moveType: "Cavalry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}selena.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Glen",
+        startingClass: "Wyvern Lord",
+        moveType: "Flying",
+        game: "Sacred Stones",
+        img1: `${sacredStones}glen.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Hayden",
+        startingClass: "Ranger",
+        moveType: "Cavalry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}hayden.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Valter",
+        startingClass: "Wyvern Knight",
+        moveType: "Flying",
+        game: "Sacred Stones",
+        img1: `${sacredStones}valter.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Fado",
+        startingClass: "General",
+        moveType: "Armor",
+        game: "Sacred Stones",
+        img1: `${sacredStones}fado.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Lyon",
+        startingClass: "Summoner",
+        moveType: "Infantry",
+        game: "Sacred Stones",
+        img1: `${sacredStones}lyon.png`,
+        img2: `${sacredStones}lyon2.png`,
+        isFound: false,
+        isPostGame: true
+    },
 ];
 
 export default FE8Units;
