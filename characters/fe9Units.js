5b450a6472a13e6251e7cd9674280f1cce17e80c
Adam Carleton <acarleton15@gmail.com>
2025-09-11 13:39:49 -0500

    finished adding fe9 characters

--- characters/fe9Units.js
+++ characters/fe9Units.js
@@ -11,14 +11,413 @@ const FE9Units = [
         isFound: false
     },
     {
+        name: "Titania",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}titania.png`,
+        isFound: false
+    },
+    {
+        name: "Oscar",
+        startingClass: "Lance Knight",
+        moveType: "Cavalry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}oscar.png`,
+        isFound: false
+    },
+    {
+        name: "Boyd",
+        startingClass: "Fighter",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}boyd.png`,
+        isFound: false
+    },
+    {
+        name: "Rhys",
+        startingClass: "Priest",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}rhys.png`,
+        isFound: false
+    },
+    {
+        name: "Shinon",
+        startingClass: "Sniper",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}shinon.png`,
+        isFound: false
+    },
+    {
+        name: "Gatrie",
+        startingClass: "Lance Armor",
+        moveType: "Armor",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}gatrie.png`,
+        isFound: false
+    },
+    {
+        name: "Soren",
+        startingClass: "Wind Mage",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}soren.png`,
+        isFound: false
+    },
+    {
+        name: "Mia",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}mia.png`,
+        isFound: false
+    },
+    {
+        name: "Ilyana",
+        startingClass: "Thunder Mage",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}ilyana.png`,
+        isFound: false
+    },
+    {
+        name: "Mist",
+        startingClass: "Cleric",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}mist.png`,
+        isFound: false
+    },
+    {
+        name: "Rolf",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}rolf.png`,
+        isFound: false
+    },
+    {
+        name: "Marcia",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}marcia.png`,
+        isFound: false
+    },
+    {
+        name: "Lethe",
+        startingClass: "Cat",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}lethe.png`,
+        isFound: false
+    },
+    {
+        name: "Mordecai",
+        startingClass: "Tiger",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}mordecai.png`,
+        isFound: false
+    },
+    {
+        name: "Volke",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}volke.png`,
+        img1: `${pathOfRadiance}volke2.png`,
+        isFound: false
+    },
+    {
+        name: "Kieran",
+        startingClass: "Axe Knight",
+        moveType: "Cavalry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}kieran.png`,
+        isFound: false
+    },
+    {
+        name: "Brom",
+        startingClass: "Axe Armor",
+        moveType: "Armor",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}brom.png`,
+        isFound: false
+    },
+    {
+        name: "Nephenee",
+        startingClass: "Soldier",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}nephenee.png`,
+        isFound: false
+    },
+    {
+        name: "Zihark",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}zihark.png`,
+        isFound: false
+    },
+    {
+        name: "Sothe",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}sothe.png`,
+        isFound: false
+    },
+    {
+        name: "Jill",
+        startingClass: "Wyvern Rider",
+        moveType: "Flying",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}jill.png`,
+        isFound: false
+    },
+    {
+        name: "Astrid",
+        startingClass: "Bow Knight",
+        moveType: "Cavalry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}astrid.png`,
+        isFound: false
+    },
+    {
+        name: "Makalov",
+        startingClass: "Sword Knight",
+        moveType: "Cavalry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}makalov.png`,
+        isFound: false
+    },
+    {
+        name: "Stefan",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}stefan.png`,
+        isFound: false
+    },
+    {
+        name: "Tormod",
+        startingClass: "Fire Mage",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}tormod.png`,
+        isFound: false
+    },
+    {
+        name: "Muarim",
+        startingClass: "Tiger",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}muarim.png`,
+        isFound: false
+    },
+    {
+        name: "Devdan",
+        startingClass: "Soldier",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}devdan.png`,
+        isFound: false
+    },
+    {
+        name: "Tanith",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}tanith.png`,
+        isFound: false
+    },
+    {
+        name: "Reyson",
+        startingClass: "Heron",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}reyson.png`,
+        isFound: false
+    },
+    {
+        name: "Janaff",
+        startingClass: "Hawk",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}janaff.png`,
+        isFound: false
+    },
+    {
+        name: "Ulki",
+        startingClass: "Hawk",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}ulki.png`,
+        isFound: false
+    },
+    {
+        name: "Calill",
+        startingClass: "Fire Sage",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}calill.png`,
+        isFound: false
+    },
+    {
+        name: "Tauroneo",
+        startingClass: "Lance General",
+        moveType: "Armor",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}tauroneo.png`,
+        isFound: false
+    },
+    {
+        name: "Haar",
+        startingClass: "Wyvern Lord",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}haar.png`,
+        isFound: false
+    },
+    {
+        name: "Ranulf",
+        startingClass: "Cat",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}ranulf.png`,
+        isFound: false
+    },
+    {
+        name: "Bastian",
+        startingClass: "Wind Sage",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}bastian.png`,
+        isFound: false
+    },
+    {
+        name: "Lucia",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}lucia.png`,
+        isFound: false
+    },
+    {
+        name: "Geoffrey",
+        startingClass: "Lance Paladin",
+        moveType: "Cavalry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}geoffrey.png`,
+        isFound: false
+    },
+    {
+        name: "Largo",
+        startingClass: "Berserker",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}largo.png`,
+        isFound: false
+    },
+    {
         name: "Elincia",
-        startingClass: "Princess",
+        startingClass: "Princess Crimea",
         moveType: "Flying",
         game: "Path of Radiance",
         img1: `${pathOfRadiance}elincia.png`,
         img2: `${pathOfRadiance}elincia2.png`,
         isFound: false
-    }
+    },
+    {
+        name: "Nasir",
+        startingClass: "White Dragon",
+        moveType: "Dragon",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}nasir.png`,
+        isFound: false
+    },
+    {
+        name: "Ena",
+        startingClass: "Red Dragon",
+        moveType: "Dragon",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}ena.png`,
+        isFound: false
+    },
+    {
+        name: "Tibarn",
+        startingClass: "Hawk King",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}tibarn.png`,
+        isFound: false
+    },
+    {
+        name: "Naesala",
+        startingClass: "Raven King",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}naesala.png`,
+        isFound: false
+    },
+    {
+        name: "Giffca",
+        startingClass: "Lion",
+        moveType: "Beast",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}giffca.png`,
+        isFound: false
+    },
+    {
+        name: "Oliver",
+        startingClass: "Bishop",
+        moveType: "Infantry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}oliver.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Shiharam",
+        startingClass: "Wyvern Lord",
+        moveType: "Flying",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}shiharam.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Petrine",
+        startingClass: "Lance Paladin",
+        moveType: "Cavalry",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}petrine.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Bryce",
+        startingClass: "Axe General",
+        moveType: "Armor",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}bryce.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    {
+        name: "Ashnard",
+        startingClass: "King",
+        moveType: "Flying",
+        game: "Path of Radiance",
+        img1: `${pathOfRadiance}ashnard.png`,
+        isFound: false,
+        isPostGame: true
+    },
+    
 ];
 
 export default FE9Units;
