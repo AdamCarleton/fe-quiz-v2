bb573947376184d5ed709963927185275063b5ff
Adam Carleton <acarleton15@gmail.com>
2025-09-12 15:52:47 -0500

    finished adding fe13 characters

--- characters/fe13Units.js
+++ characters/fe13Units.js
@@ -19,6 +19,232 @@ const FE13Units = [
         isFound: false
     },
     {
+        name: "Lissa",
+        startingClass: "Cleric",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}lissa.png`,
+        isFound: false
+    },
+    {
+        name: "Frederick",
+        startingClass: "Great Knight",
+        moveType: "Cavalry",
+        game: "Awakening",
+        img1: `${awakening}frederick.png`,
+        isFound: false
+    },
+    {
+        name: "Sully",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Awakening",
+        img1: `${awakening}sully.png`,
+        isFound: false
+    },
+    {
+        name: "Virion",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}virion.png`,
+        isFound: false
+    },
+    {
+        name: "Stahl",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Awakening",
+        img1: `${awakening}stahl.png`,
+        isFound: false
+    },
+    {
+        name: "Vaike",
+        startingClass: "Fighter",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}vaike.png`,
+        isFound: false
+    },
+    {
+        name: "Miriel",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}miriel.png`,
+        isFound: false
+    },
+    {
+        name: "Sumia",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Awakening",
+        img1: `${awakening}sumia.png`,
+        isFound: false
+    },
+    {
+        name: "Kellam",
+        startingClass: "Knight",
+        moveType: "Armor",
+        game: "Awakening",
+        img1: `${awakening}kellam.png`,
+        isFound: false
+    },
+    {
+        name: "Donnel",
+        startingClass: "Villager",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}donnel.png`,
+        isFound: false
+    },
+    {
+        name: "Lonqu",
+        displayName: "Lon'qu",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}lonqu.png`,
+        isFound: false
+    },
+    {
+        name: "Ricken",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}ricken.png`,
+        isFound: false
+    },
+    {
+        name: "Maribelle",
+        startingClass: "Troubadour",
+        moveType: "Cavalry",
+        game: "Awakening",
+        img1: `${awakening}maribelle.png`,
+        isFound: false
+    },
+    {
+        name: "Panne",
+        startingClass: "Taguel",
+        moveType: "Beast",
+        game: "Awakening",
+        img1: `${awakening}panne.png`,
+        isFound: false
+    },
+    {
+        name: "Gaius",
+        startingClass: "Thief",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}gaius.png`,
+        isFound: false
+    },
+    {
+        name: "Cordelia",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Awakening",
+        img1: `${awakening}cordelia.png`,
+        isFound: false
+    },
+    {
+        name: "Gregor",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}gregor.png`,
+        isFound: false
+    },
+    {
+        name: "Nowi",
+        startingClass: "Manakete",
+        moveType: "Dragon",
+        game: "Awakening",
+        img1: `${awakening}nowi.png`,
+        isFound: false
+    },
+    {
+        name: "Libra",
+        startingClass: "War Monk",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}libra.png`,
+        isFound: false
+    },
+    {
+        name: "Tharja",
+        startingClass: "Dark Mage",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}tharja.png`,
+        isFound: false
+    },
+    {
+        name: "Anna",
+        startingClass: "Trickster",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}anna.png`,
+        isFound: false
+    },
+    {
+        name: "Olivia",
+        startingClass: "Dancer",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}olivia.png`,
+        isFound: false
+    },
+    {
+        name: "Cherche",
+        startingClass: "Wyvern Rider",
+        moveType: "Flying",
+        game: "Awakening",
+        img1: `${awakening}cherche.png`,
+        isFound: false
+    },
+    {
+        name: "Henry",
+        startingClass: "Dark Mage",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}henry.png`,
+        isFound: false
+    },
+    {
+        name: "Sayri",
+        displayName: "Say'ri",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}sayri.png`,
+        isFound: false
+    },
+    {
+        name: "Tiki",
+        startingClass: "Manakete",
+        moveType: "Dragon",
+        game: "Awakening",
+        img1: `${awakening}tiki.png`,
+        isFound: false
+    },
+    {
+        name: "Basilio",
+        startingClass: "Berserker",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}basilio.png`,
+        isFound: false
+    },
+    {
+        name: "Flavia",
+        startingClass: "Hero",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}flavia.png`,
+        isFound: false
+    },
+    {
         name: "Lucina",
         startingClass: "Lord",
         moveType: "Infantry",
@@ -26,7 +252,153 @@ const FE13Units = [
         img1: `${awakening}lucina.png`,
         img2: `${awakening}lucina2.png`,
         isFound: false
-    }
+    },
+    {
+        name: "Owain",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}owain.png`,
+        isFound: false
+    },
+    {
+        name: "Inigo",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}inigo.png`,
+        isFound: false
+    },
+    {
+        name: "Brady",
+        startingClass: "Priest",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}brady.png`,
+        isFound: false
+    },
+    {
+        name: "Kjelle",
+        startingClass: "Knight",
+        moveType: "Armor",
+        game: "Awakening",
+        img1: `${awakening}kjelle.png`,
+        isFound: false
+    },
+    {
+        name: "Cynthia",
+        startingClass: "Pegasus Knight",
+        moveType: "Flying",
+        game: "Awakening",
+        img1: `${awakening}cynthia.png`,
+        isFound: false
+    },
+    {
+        name: "Severa",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}severa.png`,
+        isFound: false
+    },
+    {
+        name: "Gerome",
+        startingClass: "Wyvern Knight",
+        moveType: "Flying",
+        game: "Awakening",
+        img1: `${awakening}gerome.png`,
+        isFound: false
+    },
+    {
+        name: "Morgan",
+        startingClass: "Other",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}morgan.png`,
+        img2: `${awakening}morgan2.png`,
+        isFound: false
+    },
+    {
+        name: "Yarne",
+        startingClass: "Taguel",
+        moveType: "Beast",
+        game: "Awakening",
+        img1: `${awakening}yarne.png`,
+        isFound: false
+    },
+    {
+        name: "Laurent",
+        startingClass: "Mage",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}laurent.png`,
+        isFound: false
+    },
+    {
+        name: "Noire",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}noire.png`,
+        isFound: false
+    },
+    {
+        name: "Nah",
+        startingClass: "Manakete",
+        moveType: "Dragon",
+        game: "Awakening",
+        img1: `${awakening}nah.png`,
+        isFound: false
+    },
+    {
+        name: "Gangrel",
+        startingClass: "Trickster",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}gangrel.png`,
+        isFound: false
+    },
+    {
+        name: "Walhart",
+        startingClass: "Conqueror",
+        moveType: "Cavalry",
+        game: "Awakening",
+        img1: `${awakening}walhart.png`,
+        isFound: false
+    },
+    {
+        name: "Emmeryn",
+        startingClass: "Sage",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}emmeryn.png`,
+        isFound: false
+    },
+    {
+        name: "Yenfay",
+        displayName: "Yen'Fay",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}yenfay.png`,
+        isFound: false
+    },
+    {
+        name: "Aversa",
+        startingClass: "Dark Flier",
+        moveType: "Flying",
+        game: "Awakening",
+        img1: `${awakening}aversa.png`,
+        isFound: false
+    },
+    {
+        name: "Priam",
+        startingClass: "Hero",
+        moveType: "Infantry",
+        game: "Awakening",
+        img1: `${awakening}priam.png`,
+        isFound: false
+    },
 ];
 
 export default FE13Units;
