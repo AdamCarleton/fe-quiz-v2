354c13af40ecc2a13146d0815d967b27e9467ee2
Adam Carleton <acarleton15@gmail.com>
2025-09-12 15:09:23 -0500

    finished adding fe10 characters

--- characters/fe10Units.js
+++ characters/fe10Units.js
@@ -12,6 +12,298 @@ const FE10Units = [
         isFound: false
     },
     {
+        name: "Edward",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}edward.png`,
+        isFound: false
+    },
+    {
+        name: "Leonardo",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}leonardo.png`,
+        isFound: false
+    },
+    {
+        name: "Nolan",
+        startingClass: "Fighter",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}nolan.png`,
+        isFound: false
+    },
+    {
+        name: "Laura",
+        startingClass: "Cleric",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}laura.png`,
+        isFound: false
+    },
+    {
+        name: "Sothe",
+        startingClass: "Rogue",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}sothe.png`,
+        img2: `${radiantDawn}sothe2.png`,
+        isFound: false
+    },
+    {
+        name: "Ilyana",
+        startingClass: "Thunder Mage",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}ilyana.png`,
+        isFound: false
+    },
+    {
+        name: "Aran",
+        startingClass: "Soldier",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}aran.png`,
+        isFound: false
+    },
+    {
+        name: "Meg",
+        startingClass: "Axe Armor",
+        moveType: "Armor",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}meg.png`,
+        isFound: false
+    },
+    {
+        name: "Volug",
+        startingClass: "Wolf",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}volug.png`,
+        isFound: false
+    },
+    {
+        name: "Tauroneo",
+        startingClass: "Lance General",
+        moveType: "Armor",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}tauroneo.png`,
+        isFound: false
+    },
+    {
+        name: "Zihark",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}zihark.png`,
+        isFound: false
+    },
+    {
+        name: "Jill",
+        startingClass: "Dracoknight",
+        moveType: "Flying",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}jill.png`,
+        isFound: false
+    },
+    {
+        name: "Fiona",
+        startingClass: "Lance Knight",
+        moveType: "Cavalry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}fiona.png`,
+        isFound: false
+    },
+    {
+        name: "Tormod",
+        startingClass: "Fire Mage",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}tormod.png`,
+        isFound: false
+    },
+    {
+        name: "Muarim",
+        startingClass: "Tiger",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}muarim.png`,
+        isFound: false
+    },
+    {
+        name: "Vika",
+        startingClass: "Raven",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}vika.png`,
+        isFound: false
+    },
+    {
+        name: "Nailah",
+        startingClass: "Wolf Queen",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}nailah.png`,
+        isFound: false
+    },
+    {
+        name: "Rafiel",
+        startingClass: "Heron",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}rafiel.png`,
+        isFound: false
+    },
+    {
+        name: "BlackKnight",
+        displayName: "Black Knight",
+        startingClass: "Black Knight",
+        moveType: "Armor",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}blackknight.png`,
+        isFound: false
+    },
+    {
+        name: "Elincia",
+        startingClass: "Queen",
+        moveType: "Flying",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}elincia.png`,
+        isFound: false
+    },
+    {
+        name: "Marcia",
+        startingClass: "Falcon Knight",
+        moveType: "Flying",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}marcia.png`,
+        isFound: false
+    },
+    {
+        name: "Nealuchi",
+        startingClass: "Raven",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}nealuchi.png`,
+        isFound: false
+    },
+    {
+        name: "Leanne",
+        startingClass: "Heron",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}leanne.png`,
+        isFound: false
+    },
+    {
+        name: "Haar",
+        startingClass: "Dragon Lord",
+        moveType: "Flying",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}haar.png`,
+        isFound: false
+    },
+    {
+        name: "Brom",
+        startingClass: "Axe General",
+        moveType: "Armor",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}brom.png`,
+        isFound: false
+    },
+    {
+        name: "Nephenee",
+        startingClass: "Halberdier",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}nephenee.png`,
+        isFound: false
+    },
+    {
+        name: "Heather",
+        startingClass: "Rogue",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}heather.png`,
+        isFound: false
+    },
+    {
+        name: "Lucia",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}lucia.png`,
+        img2: `${radiantDawn}lucia2.png`,
+        isFound: false
+    },
+    {
+        name: "Lethe",
+        startingClass: "Cat",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}lethe.png`,
+        isFound: false
+    },
+    {
+        name: "Mordecai",
+        startingClass: "Tiger",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}mordecai.png`,
+        isFound: false
+    },
+    {
+        name: "Geoffrey",
+        startingClass: "Lance Paladin",
+        moveType: "Cavalry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}geoffrey.png`,
+        isFound: false
+    },
+    {
+        name: "Kieran",
+        startingClass: "Axe Paladin",
+        moveType: "Cavalry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}kieran.png`,
+        isFound: false
+    },
+    {
+        name: "Makalov",
+        startingClass: "Sword Paladin",
+        moveType: "Cavalier",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}makalov.png`,
+        isFound: false
+    },
+    {
+        name: "Astrid",
+        startingClass: "Bow Paladin",
+        moveType: "Cavalry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}astrid.png`,
+        isFound: false
+    },
+    {
+        name: "Devdan",
+        displayName: "Danved",
+        startingClass: "Halberdier",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}danved.png`,
+        isFound: false
+    },
+    {
+        name: "Calill",
+        startingClass: "Fire Sage",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}calill.png`,
+        isFound: false
+    },
+    {
         name: "Ike",
         startingClass: "Hero",
         moveType: "Infantry",
@@ -19,7 +311,288 @@ const FE10Units = [
         img1: `${radiantDawn}ike.png`,
         img2: `${radiantDawn}ike2.png`,
         isFound: false
-    }
+    },
+    {
+        name: "Titania",
+        startingClass: "Axe Paladin",
+        moveType: "Cavalry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}titania.png`,
+        isFound: false
+    },
+    {
+        name: "Soren",
+        startingClass: "Wind Sage",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}soren.png`,
+        isFound: false
+    },
+    {
+        name: "Mist",
+        startingClass: "Troubadour",
+        moveType: "Cavalry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}mist.png`,
+        isFound: false
+    },
+    {
+        name: "Rolf",
+        startingClass: "Sniper",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}rolf.png`,
+        isFound: false
+    },
+    {
+        name: "Boyd",
+        startingClass: "Warrior",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}boyd.png`,
+        isFound: false
+    },
+    {
+        name: "Oscar",
+        startingClass: "Lance Paladin",
+        moveType: "Cavalry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}oscar.png`,
+        isFound: false
+    },
+    {
+        name: "Shinon",
+        startingClass: "Sniper",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}shinon.png`,
+        isFound: false
+    },
+    {
+        name: "Gatrie",
+        startingClass: "Lance General",
+        moveType: "Armor",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}gatrie.png`,
+        isFound: false
+    },
+    {
+        name: "Rhys",
+        startingClass: "Bishop",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}rhys.png`,
+        isFound: false
+    },
+    {
+        name: "Mia",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}mia.png`,
+        isFound: false
+    },
+    {
+        name: "Ranulf",
+        startingClass: "Cat",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}ranulf.png`,
+        isFound: false
+    },
+    {
+        name: "Kyza",
+        startingClass: "Tiger",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}kyza.png`,
+        isFound: false
+    },
+    {
+        name: "Lyre",
+        startingClass: "Cat",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}lyre.png`,
+        isFound: false
+    },
+    {
+        name: "Reyson",
+        startingClass: "Heron",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}reyson.png`,
+        isFound: false
+    },
+    {
+        name: "Janaff",
+        startingClass: "Hawk",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}janaff.png`,
+        isFound: false
+    },
+    {
+        name: "Ulki",
+        startingClass: "Hawk",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}ulki.png`,
+        isFound: false
+    },
+    {
+        name: "Sigrun",
+        startingClass: "Falcon Knight",
+        moveType: "Flying",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}sigrun.png`,
+        isFound: false
+    },
+    {
+        name: "Tanith",
+        startingClass: "Falcon Knight",
+        moveType: "Flying",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}tanith.png`,
+        isFound: false
+    },
+    {
+        name: "Skrimir",
+        startingClass: "Lion",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}skrimir.png`,
+        isFound: false
+    },
+    {
+        name: "Naesala",
+        startingClass: "Raven King",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}naesala.png`,
+        isFound: false
+    },
+    {
+        name: "Sanaki",
+        startingClass: "Empress",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}sanaki.png`,
+        isFound: false
+    },
+    {
+        name: "Tibarn",
+        startingClass: "Hawk King",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}tibarn.png`,
+        isFound: false
+    },
+    {
+        name: "Pelleas",
+        startingClass: "Dark Sage",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}pelleas.png`,
+        isFound: false
+    },
+    {
+        name: "Stefan",
+        startingClass: "Trueblade",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}stefan.png`,
+        isFound: false
+    },
+    {
+        name: "Oliver",
+        startingClass: "Saint",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}oliver.png`,
+        isFound: false
+    },
+    {
+        name: "Bastian",
+        startingClass: "Archsage",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}bastian.png`,
+        isFound: false
+    },
+    {
+        name: "Volke",
+        startingClass: "Assassin",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}volke.png`,
+        isFound: false
+    },
+    {
+        name: "Caineghis",
+        startingClass: "Lion King",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}caineghis.png`,
+        isFound: false
+    },
+    {
+        name: "Giffca",
+        startingClass: "Lion",
+        moveType: "Beast",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}giffca.png`,
+        isFound: false
+    },
+    {
+        name: "Kurthnaga",
+        startingClass: "Dragon Prince",
+        moveType: "Dragon",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}kurthnaga.png`,
+        isFound: false
+    },
+    {
+        name: "Ena",
+        startingClass: "Red Dragon",
+        moveType: "Dragon",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}ena.png`,
+        isFound: false
+    },
+    {
+        name: "Renning",
+        startingClass: "Gold Knight",
+        moveType: "Cavalry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}renning.png`,
+        isFound: false
+    },
+    {
+        name: "Gareth",
+        startingClass: "Red Dragon",
+        moveType: "Dragon",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}gareth.png`,
+        isFound: false
+    },
+    {
+        name: "Nasir",
+        startingClass: "White Dragon",
+        moveType: "Dragon",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}nasir.png`,
+        isFound: false
+    },
+    {
+        name: "Lehran",
+        startingClass: "Chancellor",
+        moveType: "Infantry",
+        game: "Radiant Dawn",
+        img1: `${radiantDawn}lehran.png`,
+        isFound: false
+    },
+
 ];
 
 export default FE10Units;
