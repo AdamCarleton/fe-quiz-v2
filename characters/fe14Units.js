e2953b7f69ae0bca2f8b8116712d7f289553c79d
Adam Carleton <acarleton15@gmail.com>
2025-09-13 16:26:23 -0500

    finished adding fe14 characters

--- characters/fe14Units.js
+++ characters/fe14Units.js
@@ -9,7 +9,553 @@ const FE14Units = [
         img1: `${fates}corrin.png`,
         img2: `${fates}corrin2.png`,
         isFound: false
-    }
+    },
+    {
+        name: "Gunter",
+        startingClass: "Great Knight",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}gunter.png`,
+        isFound: false
+    },
+    {
+        name: "Felicia",
+        startingClass: "Maid",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}felicia.png`,
+        isFound: false
+    },
+    {
+        name: "Jakob",
+        startingClass: "Butler",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}jakob.png`,
+        isFound: false
+    },
+    {
+        name: "Rinkah",
+        startingClass: "Oni Savage",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}rinkah.png`,
+        isFound: false
+    },
+    {
+        name: "Kaze",
+        startingClass: "Ninja",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}kaze.png`,
+        isFound: false
+    },
+    {
+        name: "Sakura",
+        startingClass: "Shrine Maiden",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}sakura.png`,
+        isFound: false
+    },
+    {
+        name: "Azura",
+        startingClass: "Songstress",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}azura.png`,
+        isFound: false
+    },
+    {
+        name: "Hana",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}hana.png`,
+        isFound: false
+    },
+    {
+        name: "Subaki",
+        startingClass: "Sky Knight",
+        moveType: "Flying",
+        game: "Fates",
+        img1: `${fates}subaki.png`,
+        isFound: false
+    },
+    {
+        name: "Silas",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}silas.png`,
+        isFound: false
+    },
+    {
+        name: "Saizo",
+        startingClass: "Ninja",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}saizo.png`,
+        isFound: false
+    },
+    {
+        name: "Orochi",
+        startingClass: "Diviner",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}orochi.png`,
+        isFound: false
+    },
+    {
+        name: "Hinoka",
+        startingClass: "Sky Knight",
+        moveType: "Flying",
+        game: "Fates",
+        img1: `${fates}hinoka.png`,
+        isFound: false
+    },
+    {
+        name: "Azama",
+        startingClass: "Monk",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}azama.png`,
+        isFound: false
+    },
+    {
+        name: "Setsuna",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}setsuna.png`,
+        isFound: false
+    },
+    {
+        name: "Hayato",
+        startingClass: "Diviner",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}hayato.png`,
+        isFound: false
+    },
+    {
+        name: "Oboro",
+        startingClass: "Spear Fighter",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}oboro.png`,
+        isFound: false
+    },
+    {
+        name: "Hinata",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}hinata.png`,
+        isFound: false
+    },
+    {
+        name: "Takumi",
+        startingClass: "Sniper",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}takumi.png`,
+        isFound: false
+    },
+    {
+        name: "Kagero",
+        startingClass: "Ninja",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}kagero.png`,
+        isFound: false
+    },
+    {
+        name: "Reina",
+        startingClass: "Kinshi Knight",
+        moveType: "Flying",
+        game: "Fates",
+        img1: `${fates}reina.png`,
+        isFound: false
+    },
+    {
+        name: "Kaden",
+        startingClass: "Kitsune",
+        moveType: "Beast",
+        game: "Fates",
+        img1: `${fates}kaden.png`,
+        isFound: false
+    },
+    {
+        name: "Ryoma",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}ryoma.png`,
+        isFound: false
+    },
+    {
+        name: "Scarlet",
+        startingClass: "Wyvern Lord",
+        moveType: "Flying",
+        game: "Fates",
+        img1: `${fates}scarlet.png`,
+        isFound: false
+    },
+    {
+        name: "Shura",
+        startingClass: "Adventurer",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}shura.png`,
+        isFound: false
+    },
+    {
+        name: "Elise",
+        startingClass: "Troubadour",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}elise.png`,
+        isFound: false
+    },
+    {
+        name: "Arthur",
+        startingClass: "Fighter",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}arthur.png`,
+        isFound: false
+    },
+    {
+        name: "Effie",
+        startingClass: "Knight",
+        moveType: "Armor",
+        game: "Fates",
+        img1: `${fates}effie.png`,
+        isFound: false
+    },
+    {
+        name: "Odin",
+        startingClass: "Dark Mage",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}odin.png`,
+        isFound: false
+    },
+    {
+        name: "Niles",
+        startingClass: "Outlaw",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}niles.png`,
+        isFound: false
+    },
+    {
+        name: "Nyx",
+        startingClass: "Dark Mage",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}nyx.png`,
+        isFound: false
+    },
+    {
+        name: "Camilla",
+        startingClass: "Malig Knight",
+        moveType: "Flying",
+        game: "Fates",
+        img1: `${fates}camilla.png`,
+        isFound: false
+    },
+    {
+        name: "Selena",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}selena.png`,
+        isFound: false
+    },
+    {
+        name: "Beruka",
+        startingClass: "Wyvern Rider",
+        moveType: "Flying",
+        game: "Fates",
+        img1: `${fates}beruka.png`,
+        isFound: false
+    },
+    {
+        name: "Laslow",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}laslow.png`,
+        isFound: false
+    },
+    {
+        name: "Peri",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}peri.png`,
+        isFound: false
+    },
+    {
+        name: "Benny",
+        startingClass: "Knight",
+        moveType: "Armor",
+        game: "Fates",
+        img1: `${fates}benny.png`,
+        isFound: false
+    },
+    {
+        name: "Charlotte",
+        startingClass: "Fighter",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}charlotte.png`,
+        isFound: false
+    },
+    {
+        name: "Leo",
+        startingClass: "Dark Knight",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}leo.png`,
+        isFound: false
+    },
+    {
+        name: "Keaton",
+        startingClass: "Wolfskin",
+        moveType: "Beast",
+        game: "Fates",
+        img1: `${fates}keaton.png`,
+        isFound: false
+    },
+    {
+        name: "Xander",
+        startingClass: "Paladin",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}xander.png`,
+        isFound: false
+    },
+    {
+        name: "Izana",
+        startingClass: "Onmyoji",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}izana.png`,
+        isFound: false
+    },
+    {
+        name: "Yukimura",
+        startingClass: "Mechanist",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}yukimura.png`,
+        isFound: false
+    },
+    {
+        name: "Flora",
+        startingClass: "Maid",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}flora.png`,
+        isFound: false
+    },
+    {
+        name: "Fuga",
+        startingClass: "Master of Arms",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}fuga.png`,
+        isFound: false
+    },
+    {
+        name: "Mozu",
+        startingClass: "Villager",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}mozu.png`,
+        isFound: false
+    },
+    {
+        name: "Kana",
+        startingClass: "Manakete",
+        moveType: "Dragon",
+        game: "Fates",
+        img1: `${fates}kana.png`,
+        img2: `${fates}kana2.png`,
+        isFound: false
+    },
+    {
+        name: "Shigure",
+        startingClass: "Sky Knight",
+        moveType: "Flying",
+        game: "Fates",
+        img1: `${fates}shigure.png`,
+        isFound: false
+    },
+    {
+        name: "Dwyer",
+        startingClass: "Troubadour",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}dwyer.png`,
+        isFound: false
+    },
+    {
+        name: "Sophie",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}sophie.png`,
+        isFound: false
+    },
+    {
+        name: "Midori",
+        startingClass: "Apothecary",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}midori.png`,
+        isFound: false
+    },
+    {
+        name: "Shiro",
+        startingClass: "Spear Fighter",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}shiro.png`,
+        isFound: false
+    },
+    {
+        name: "Kiragi",
+        startingClass: "Archer",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}kiragi.png`,
+        isFound: false
+    },
+    {
+        name: "Asugi",
+        startingClass: "Ninja",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}asugi.png`,
+        isFound: false
+    },
+    {
+        name: "Selkie",
+        startingClass: "Kitsune",
+        moveType: "Beast",
+        game: "Fates",
+        img1: `${fates}selkie.png`,
+        isFound: false
+    },
+    {
+        name: "Hisame",
+        startingClass: "Myrmidon",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}hisame.png`,
+        isFound: false
+    },
+    {
+        name: "Mitama",
+        startingClass: "Shrine Maiden",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}mitama.png`,
+        isFound: false
+    },
+    {
+        name: "Caeldori",
+        startingClass: "Sky Knight",
+        moveType: "Flying",
+        game: "Fates",
+        img1: `${fates}caeldori.png`,
+        isFound: false
+    },
+    {
+        name: "Rhajat",
+        startingClass: "diviner",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}rhajat.png`,
+        isFound: false
+    },
+    {
+        name: "Siegbert",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}siegbert.png`,
+        isFound: false
+    },
+    {
+        name: "Forrest",
+        startingClass: "Troubadour",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}forrest.png`,
+        isFound: false
+    },
+    {
+        name: "Ignatius",
+        startingClass: "Cavalier",
+        moveType: "Cavalry",
+        game: "Fates",
+        img1: `${fates}ignatius.png`,
+        isFound: false
+    },
+    {
+        name: "Velouria",
+        startingClass: "Wolfskin",
+        moveType: "Beast",
+        game: "Fates",
+        img1: `${fates}velouria.png`,
+        isFound: false
+    },
+    {
+        name: "Percy",
+        startingClass: "Wyvern Rider",
+        moveType: "Flying",
+        game: "Fates",
+        img1: `${fates}percy.png`,
+        isFound: false
+    },
+    {
+        name: "Ophelia",
+        startingClass: "Dark Mage",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}ophelia.png`,
+        isFound: false
+    },
+    {
+        name: "Soleil",
+        startingClass: "Mercenary",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}soleil.png`,
+        isFound: false
+    },
+    {
+        name: "Nina",
+        startingClass: "Outlaw",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}nina.png`,
+        isFound: false
+    },
+    {
+        name: "Anna",
+        startingClass: "Outlaw",
+        moveType: "Infantry",
+        game: "Fates",
+        img1: `${fates}anna.png`,
+        isFound: false
+    },
+    
 ];
 
 export default FE14Units;
