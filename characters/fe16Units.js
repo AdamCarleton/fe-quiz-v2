12d8f1f170823b48eae908376ac2e7d8126a7366
Adam Carleton <acarleton15@gmail.com>
2025-09-14 14:07:38 -0500

    finished adding fe16 characters

--- characters/fe16Units.js
+++ characters/fe16Units.js
@@ -22,13 +22,66 @@ const FE16Units = [
         isFound: false
     },
     {
-        name: "Dimitri",
+        name: "Hubert",
         startingClass: "Student",
         moveType: "Infantry",
         game: "Three Houses",
-        img1: `${threeHouses}dimitri.png`,
-        img2: `${threeHouses}dimitri2.png`,
-        img3: `${threeHouses}dimitri3.png`,
+        img1: `${threeHouses}hubert.png`,
+        img2: `${threeHouses}hubert2.png`,
+        isFound: false
+    },
+    {
+        name: "Bernadetta",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}bernadetta.png`,
+        img2: `${threeHouses}bernadetta2.png`,
+        isFound: false
+    },
+    {
+        name: "Ferdinand",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}ferdinand.png`,
+        img2: `${threeHouses}ferdinand2.png`,
+        isFound: false
+    },
+    {
+        name: "Dorothea",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}dorothea.png`,
+        img2: `${threeHouses}dorothea2.png`,
+        isFound: false
+    },
+    {
+        name: "Caspar",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}caspar.png`,
+        img2: `${threeHouses}caspar2.png`,
+        isFound: false
+    },
+    {
+        name: "Petra",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}petra.png`,
+        img2: `${threeHouses}petra2.png`,
+        isFound: false
+    },
+    {
+        name: "Linhardt",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}linhardt.png`,
+        img2: `${threeHouses}linhardt2.png`,
         isFound: false
     },
     {
@@ -39,7 +92,279 @@ const FE16Units = [
         img1: `${threeHouses}claude.png`,
         img2: `${threeHouses}claude2.png`,
         isFound: false
-    }
+    },
+    {
+        name: "Hilda",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}hilda.png`,
+        img2: `${threeHouses}hilda2.png`,
+        isFound: false
+    },
+    {
+        name: "Lorenz",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}lorenz.png`,
+        img2: `${threeHouses}lorenz2.png`,
+        isFound: false
+    },
+    {
+        name: "Raphael",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}raphael.png`,
+        img2: `${threeHouses}raphael2.png`,
+        isFound: false
+    },
+    {
+        name: "Lysithea",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}lysithea.png`,
+        img2: `${threeHouses}lysithea2.png`,
+        isFound: false
+    },
+    {
+        name: "Ignatz",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}ignatz.png`,
+        img2: `${threeHouses}ignatz2.png`,
+        isFound: false
+    },
+    {
+        name: "Marianne",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}marianne.png`,
+        img2: `${threeHouses}marianne2.png`,
+        isFound: false
+    },
+    {
+        name: "Leonie",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}leonie.png`,
+        img2: `${threeHouses}leonie2.png`,
+        isFound: false
+    },
+    {
+        name: "Dimitri",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}dimitri.png`,
+        img2: `${threeHouses}dimitri2.png`,
+        img3: `${threeHouses}dimitri3.png`,
+        isFound: false
+    },
+    {
+        name: "Dedue",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}dedue.png`,
+        img2: `${threeHouses}dedue2.png`,
+        isFound: false
+    },
+    {
+        name: "Felix",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}felix.png`,
+        img2: `${threeHouses}felix2.png`,
+        isFound: false
+    },
+    {
+        name: "Mercedes",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}mercedes.png`,
+        img2: `${threeHouses}mercedes2.png`,
+        isFound: false
+    },
+    {
+        name: "Ashe",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}ashe.png`,
+        img2: `${threeHouses}ashe2.png`,
+        isFound: false
+    },
+    {
+        name: "Annette",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}annette.png`,
+        img2: `${threeHouses}annette2.png`,
+        isFound: false
+    },
+    {
+        name: "Sylvain",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}sylvain.png`,
+        img2: `${threeHouses}sylvain2.png`,
+        isFound: false
+    },
+    {
+        name: "Ingrid",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}ingrid.png`,
+        img2: `${threeHouses}ingrid2.png`,
+        isFound: false
+    },
+    {
+        name: "Hanneman",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}hanneman.png`,
+        isFound: false
+    },
+    {
+        name: "Manuela",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}manuela.png`,
+        isFound: false
+    },
+    {
+        name: "Gilbert",
+        startingClass: "General",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}gilbert.png`,
+        isFound: false
+    },
+    {
+        name: "Catherine",
+        startingClass: "Swordmaster",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}catherine.png`,
+        isFound: false
+    },
+    {
+        name: "Alois",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}alois.png`,
+        isFound: false
+    },
+    {
+        name: "Shamir",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}shamir.png`,
+        isFound: false
+    },
+    {
+        name: "Seteth",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}seteth.png`,
+        isFound: false
+    },
+    {
+        name: "Flayn",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}flayn.png`,
+        isFound: false
+    },
+    {
+        name: "Cyril",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}cyril.png`,
+        img2: `${threeHouses}cyril2.png`,
+        isFound: false
+    },
+    {
+        name: "Yuri",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}yuri.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Balthus",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}balthus.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Constance",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}constance.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Hapi",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}hapi.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Jeritza",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}jeritza.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Anna",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}anna.png`,
+        isFound: false,
+        isDLC: true
+    },
+    {
+        name: "Aelfric",
+        startingClass: "Student",
+        moveType: "Infantry",
+        game: "Three Houses",
+        img1: `${threeHouses}aelfric.png`,
+        isFound: false,
+        isDLC: true
+    },
 ];
 
 export default FE16Units;
